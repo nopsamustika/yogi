0ca66f50e80930b47afa16ab58f17c86050cf4d7
nopsamustika <nopsamustika4@gmail.com>
2025-12-01 19:04:15 +0700

    upload project

---
+++ assets/js/data-project.js
@@ -0,0 +1,93 @@
+// assets/js/data-project.js
+window.projectData = [
+  {
+    id: 1,
+    judul: "Siaga Siber",
+    kategori: "Website",
+    tahun: 2024,
+    link: "https://nopsamustika.github.io/siaga-cyber/",
+    deskripsi: "Pembuatan UI aplikasi pembelajaran berbasis web untuk anak sekolah dasar tentang cybersecurity.",
+    gambar: ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg", "7.jpeg"]
+  },
+  {
+    id: 2,
+    judul: "Toko Pancing",
+    kategori: "APP",
+    tahun: 2022,
+    link: "https://www.figma.com/proto/Lu0nST9Rhl5LCOLraomb4g/Tugas-Teknoprener?node-id=1-2&p=f&t=dx0b6s7umLgkzCTN-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A2",
+    deskripsi: "Pembuatan UI aplikasi berbasis mobile untuk penjualan perlengkapan mancing dalam bentuk prototype.",
+    gambar: ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg"]
+  },
+  {
+    id: 3,
+    judul: "LyDer Apps",
+    kategori: "APP",
+    tahun: 2023,
+    link: "https://www.figma.com/proto/5aznlFvCM1HeDnXEXbZUqe/Lyder-App?node-id=102-101&starting-point-node-id=102%3A10",
+    deskripsi: "Pembuatan UI aplikasi berbasi mobile untuk pengaduan dan diskusi mengenai bullying dalam bentuk prototype.",
+    gambar: ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg"]
+  },
+  {
+    id: 4,
+    judul: "Klasifikasi Air Sungai dan Danau",
+    kategori: "APP",
+    tahun: 2023,
+    link: "https://www.figma.com/proto/Ccif2kBBB8Ioeta9i35hMF/Klasifikasi-Air-Sungai-dan-Danau?node-id=6-96&p=f&t=4yKSf9LtWMt8Q4XC-1&scaling=contain&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A4",
+    deskripsi: "Pembuatan UI aplikasi berbasis mobile untuk mendata klasifikasi air sungai dan danau dalam bentuk prototype.",
+    gambar: ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg", "7.jpeg"]
+  },
+  {
+    id: 5,
+    judul: "Toko Penjualan Ikan Channa",
+    kategori: "APP",
+    tahun: 2023,
+    link: "https://www.figma.com/proto/BlJL43NTbgdSf0vCygJeQF/21.0504.0041_Nopsa-Mustika-P.?node-id=1-2&p=f&t=llduXTSEbkRV5o0z-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A2",
+    deskripsi: "Pembuatan UI aplikasi berbasis mobile untuk penjualan berbagai jenis ikan channa dalam bentuk prototype.",
+    gambar: ["1.jpeg", "2.jpeg", "3.jpeg"]
+  },
+  {
+    id: 6,
+    judul: "Sistem Informasi Aplikasi Penomoran (SI'AP)",
+    kategori: "Website",
+    tahun: 2024,
+    link: "https://drive.google.com/drive/folders/18o-D0eCL2O3HdCuUfwbCCsGb962QVFTz?usp=sharing",
+    deskripsi: "Pembuatan aplikasi penomoran surat berbasis web di Setda Bagian Umum Kabupaten Magelang.",
+    gambar: ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg"]
+  },
+  {
+    id: 7,
+    judul: "Perpustakaan",
+    kategori: "Website",
+    tahun: 2025,
+    link: "https://github.com/nopsamustika/perpustakaan",
+    deskripsi: "Pembuatan aplikasi peminjaman buku perpustakaan berbasis web.",
+    gambar: ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg"]
+  },
+  {
+    id: 8,
+    judul: "Booking Ruangan",
+    kategori: "Website",
+    tahun: 2025,
+    link: "https://github.com/nopsamustika/booking-ruangan",
+    deskripsi: "Pembuatan aplikasi pembookingan ruangan berbasis web.",
+    gambar: ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg"]
+  },
+  {
+    id: 9,
+    judul: "Klasifikasi Jenis Ikan Channa",
+    kategori: "Machine Learning",
+    tahun: 2024,
+    link: "https://colab.research.google.com/drive/1UP24LSzycCWleEipfhLC2mxfnE5RXeyw?usp=sharing",
+    deskripsi: "Klasifikasi jenis ikan channa berdasarkan warna dan tekstur menggunakan preprocessing citra. Model terbaik KNN dengan akurasi 0.4.",
+    gambar: ["1.jpeg", "2.jpeg", "3.jpeg"]
+  },
+  {
+    id: 10,
+    judul: "Prediksi Komponen PC",
+    kategori: "Machine Learning",
+    tahun: 2024,
+    link: "https://colab.research.google.com/drive/1zeDnaO02Io9uaUB9HouWWG2PLT8uYTeT?usp=sharing",
+    deskripsi: "Prediksi komponen PC dengan machine learning dan akurasi 89%.",
+    gambar: ["1.jpeg", "2.jpeg", "3.jpeg"]
+  }
+];
